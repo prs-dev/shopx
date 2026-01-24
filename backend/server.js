397a5238a5926d415e31564a337a4479085a5c54
prs-dev <pratyushsri.25@gmail.com>
2026-01-24 12:21:28 +0530

    mongodb connected and tested

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
 const express = require("express")
 const mongoose = require('mongoose')
+require("dotenv").config()
 const app = express()
-
-app.post('/register', (req, res) => {
-
+const User = require("./models/User")
+app.post('/register', async(req, res) => {
+    const newUser = new User({
+        name: "test",
+        email: "test",
+        password: "1234"
+    })
+    await newUser.save()
 })
 
 app.get("/", (req, res) => {
     res.send("<h1>hello</h1>")
 })
 
-app.listen(5000, () => {
-    console.log("server is running on port 5000")
-})
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("mongodb connected")
+        app.listen(5000, () => {
+            console.log("server is running on port 5000")
+        })
+    })
+    .catch(err => console.log(err))
+
