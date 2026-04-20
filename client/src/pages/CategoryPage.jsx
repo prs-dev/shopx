82f78e521de0a0714d99afe96254aed3a06cb07a
prs-dev <pratyushsri.25@gmail.com>
2026-04-20 15:52:27 +0530

    changed layout of categories page

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { userToken } from '../context/UserContext'
 import useApi from '../utils/useApi'
 
@@ -22,23 +22,23 @@ const CategoryPage = () => {
     const [state, setState] = useState()
     const token = userToken()
 
-    const {fetchCategories, createCategory, updateCategory} = useApi()
+    const { fetchCategories, createCategory, updateCategory } = useApi()
 
     const handleChange = e => {
-        if(active) {
-            setActive(prev => ({...prev, [e.target.name]: e.target.value}))
+        if (active) {
+            setActive(prev => ({ ...prev, [e.target.name]: e.target.value }))
         }
-        setState(prev => ({...prev, [e.target.name]: e.target.value}))
+        setState(prev => ({ ...prev, [e.target.name]: e.target.value }))
     }
 
     useEffect(() => {
-        if(token) {
+        if (token) {
             fetchCategories()
-            .then(data => setCategories(data))
+                .then(data => setCategories(data))
         }
     }, [])
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         createCategory(state, token)
     }
@@ -49,35 +49,54 @@ const CategoryPage = () => {
     }
 
     console.log("categories", categories, active)
-  return (
-    <div>
-        {/* category list */}
-        <div>
-            {categories?.map(item => <div>
-                <h2>{item.name}</h2>
-                <p>{item.description}</p>
-                {item.products?.map(product => <div>{product}</div>)}
-                <button onClick={() => setActive(item)}>update</button>
-                <button>delete</button>
-            </div>)}
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: "center",
+            justifyContent: "space-between"
+        }}>
+            {/* category list */}
+            <div style={{
+                padding: "20px",
+                display: 'flex',
+                flexDirection: "column",
+                flex: 1,
+                gap: "10px"
+            }}>
+                {categories?.map(item => <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "4px"
+                }}>
+                    <h2>{item.name}</h2>
+                    <p>{item.description}</p>
+                    {item.products?.map(product => <div>{product}</div>)}
+                    <div>
+                        <button onClick={() => setActive(item)}>update</button>
+                        <button>delete</button>
+                    </div>
+                </div>)}
+            </div>
+            {/* add category */}
+            <form style={{
+                padding: "20px",
+                flex: 1
+            }} action="" onSubmit={active ? handleUpdate : handleSubmit}>
+                <div>
+                    <h2>{active ? 'Update' : "Add new"} category</h2>
+                </div>
+                <div>
+                    <label htmlFor="">Name</label>
+                    <input type="text" name="name" value={active ? active?.name : state?.name} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="">Description</label>
+                    <textarea type="text" name="description" value={active ? active?.description : state?.description} onChange={handleChange} />
+                </div>
+                <div><button>{active ? "Update" : "Save"}</button></div>
+            </form>
         </div>
-        {/* add category */}
-        <form action="" onSubmit={active ? handleUpdate : handleSubmit}>
-            <div>
-                <h2>{active ? 'Update' : "Add new"} category</h2>
-            </div>
-            <div>
-                <label htmlFor="">Name</label>
-                <input type="text" name="name" value={active ? active?.name : state?.name} onChange={handleChange}/>
-            </div>
-            <div>
-                <label htmlFor="">Description</label>
-                <textarea type="text" name="description" value={active ? active?.description : state?.description} onChange={handleChange}/>
-            </div>
-            <div><button>{active ? "Update": "Save"}</button></div>
-        </form>
-    </div>
-  )
+    )
 }
 
 export default CategoryPage
