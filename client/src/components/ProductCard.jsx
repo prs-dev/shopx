4a0fa217c10f617ab624bfeb1b2ff0ed1cf1df23
prs-dev <pratyushsri.25@gmail.com>
2026-04-23 14:44:39 +0530

    added filter bar

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,18 +2,38 @@ import React from 'react'
 
 const ProductCard = ({product}) => {
   return (
-    <div>
+    <div style={{
+        border: "1px solid #333",
+        padding: "10px",
+        display: "flex",
+        flexDirection: 'column',
+        alignItems: "center",
+        justifyContent: "center",
+        width: "300px",
+        height: "200px",
+        borderRadius: "10px",
+        gap: "10px",
+        boxShadow: "2px 2px 10px rgba(0,0,0,0.5)"
+    }}>
         <div>
             <h2>{product?.name}</h2>
         </div>
         <div>
-            <p>{product?.description}</p>
+            <p style={{
+                fontSize: "14px",
+                oveflow: "hidden",
+                width: "200px"
+            }}>{product?.description}</p>
         </div>
         <div>
-            <p>Cost: {product?.price}</p>
+            <p><span style={{
+                fontWeight: 700
+            }}>Cost:</span> {product?.price}</p>
         </div>
         <div>
-            <p>Currently in stock: {product?.stock}</p>
+            <p><span style={{
+                fontWeight: 700
+            }}>Currently in stock:</span> {product?.stock}</p>
         </div>
     </div>
   )
--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -5,21 +5,58 @@ import useApi from "../utils/useApi"
 
 const ProductPage = ({role}) => {
   const [categories, setCategories] = useState(null)
+  const [activeCategory, setActiveCategory] = useState("")
+  const [filterProducts, setFilterProducts] = useState(null)
   const products = allProducts()
   const [activeProduct, setActiveProduct] = useState(null)
   const {fetchCategories} = useApi()
   const setToUpdate = useSetToUpdate()
   const updateProduct = updateExistingProduct()
+
   useEffect(() => {
-    if(role === "admin") {
+    // if(role === "admin") {
       fetchCategories()
       .then(data => setCategories(data))
-    }
+    // }
   }, [])
   // console.log("categories", categories, activeProduct)
+
+  useEffect(() => {
+    if(activeCategory) {
+      const productsFilter = products.filter(item => item.category === activeCategory)
+      setFilterProducts(productsFilter)
+    }
+  }, [activeCategory])
+
+  console.log("filter", activeCategory, filterProducts)
+
   return (
-    <div>
-      {products?.map(product => <>
+    <div style={{
+      display: "flex",
+      gap: "20px",
+      flexWrap: 'wrap',
+      padding: "5px",
+      alignItems: "center",
+      justifyContent: "center",
+      margin: "auto"
+    }}>
+      <div style={{
+        alignSelf: 'flex-start',
+        width: "100%",
+        padding: "10px",
+        border: "1px solid #333"
+      }}>
+        {/* filter bar */}
+        <select value={activeCategory} onChange={e => setActiveCategory(e.target.value)}>
+          <option value="" disabled>Select</option>
+          {categories?.map(item => <option value={item._id}>{item.name}</option>)}
+        </select>
+        <button onClick={() => {
+          setActiveCategory("")
+          setFilterProducts(null)
+        }}>Clear Filter</button>
+      </div>
+      {filterProducts ? <>{filterProducts.map(item => <ProductCard {...{product: item}}/>)}</>: products?.map(product => <>
         <ProductCard {...{product}}/>
         {role === 'admin' && <div>
           <h2>Add Category</h2>
@@ -34,7 +71,7 @@ const ProductPage = ({role}) => {
             {categories?.map(item => <option value={item._id}>{item.name}</option>)}
           </select>
         </div>}
-        <button onClick={() => updateProduct(activeProduct)}>save</button>
+        {activeProduct && <button onClick={() => updateProduct(activeProduct)}>save</button>}
       </>)}
     </div>
   )
